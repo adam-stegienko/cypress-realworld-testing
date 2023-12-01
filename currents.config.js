32bd25db3e46c779aa447b9ae73391d17359707d
Adam Stegienko <adam.stegienko1@gmail.com>
2023-12-01 17:43:29 +0100

    currents.config.js updated

--- currents.config.js
+++ currents.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    projectId: "new-demo",
+    projectId: "project-demo",
     recordKey: "ABC",
-    cloudServiceUrl: "https://director.local",
+    cloudServiceUrl: "https://director.cypress.local",
     NODE_EXTRA_CA_CERTS: "/etc/ssl/certs/demo.pem",
 };
